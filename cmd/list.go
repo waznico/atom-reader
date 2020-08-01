d0819227503f66bf2aa5772bb8a9a95dd9e741f1
Nicolas Wazulek <nicolas.wazulek@niwa-dev.de>
2020-08-01 17:49:25 +0200

    customized feeds for list
    atom-reader ls now accesses feeds.json to get all feeds and calls all uris of this json file to get newest headlines.

--- cmd/list.go
+++ cmd/list.go
@@ -4,25 +4,34 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/waznico/atom-reader/internal/pkg/config"
+
 	"github.com/jedib0t/go-pretty/table"
 
 	"github.com/mmcdole/gofeed"
 )
 
 func list() {
-	fp := gofeed.NewParser()
-	feed, err := fp.ParseURL("https://www.heise.de/rss/heise-atom.xml")
-	if err != nil {
-		fmt.Println("Feed couldn't be reached. Please check your network connection and try again.")
-	}
-	t := table.NewWriter()
-	t.SetOutputMirror(os.Stdout)
-	t.AppendHeader(table.Row{"ID", "Title"})
+	var c config.Config
 
-	for i := 0; i < 10; i++ {
-		t.AppendRows([]table.Row{
-			{feed.Items[i].GUID, feed.Items[i].Title},
-		})
+	c.LoadConfig()
+
+	for fi := range c.Feeds.Feeds {
+		fp := gofeed.NewParser()
+		feed, err := fp.ParseURL(c.Feeds.Feeds[fi].URI)
+		if err != nil {
+			fmt.Printf("Feed %v couldn't be reached. Please check your network connection and try again.\n", c.Feeds.Feeds[fi].Name)
+		}
+		t := table.NewWriter()
+		t.SetOutputMirror(os.Stdout)
+		t.SetTitle("Feeds of %v", c.Feeds.Feeds[fi].Name)
+		t.AppendHeader(table.Row{"ID", "Title"})
+
+		for i := 0; i < 10; i++ {
+			t.AppendRows([]table.Row{
+				{feed.Items[i].GUID, feed.Items[i].Title},
+			})
+		}
+		t.Render()
 	}
-	t.Render()
 }
